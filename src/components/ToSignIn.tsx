edfdc93f1e632621ef49b6fa12a7b9813320206c
itzmeowww <winthanasan@gmail.com>
2023-04-07 02:13:23 +0900

    minor fix

--- src/components/ToSignIn.tsx
+++ src/components/ToSignIn.tsx
@@ -4,10 +4,12 @@ import AppName from './AppName';
 const ToSignIn = () => {
     return (
         <Container>
-            <AppName />
-            <button className="btn mt-6" onClick={() => { void signIn() }}>
-                Sign In
-            </button>
+            <div className="flex flex-1 flex-col justify-center items-center">
+                <AppName />
+                <button className="btn mt-6" onClick={() => { void signIn() }}>
+                    Sign In
+                </button>
+            </div>
         </Container>
     );
 }
